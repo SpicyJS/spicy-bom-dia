2397516a5fb75b2b7fd3b16b7a47d50c0ee0ff95
Víctor Reyes <unhill@gmail.com>
2024-10-10 17:07:57 +0200

    Add fetch functionality
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- front-end/src/index.ts
+++ front-end/src/index.ts
@@ -1,1 +1,10 @@
 console.log("working on index.js");
+
+fetch("http://localhost:4444")
+  .then((res) => {
+    if (!res.ok) throw new Error("Not OK!");
+
+    return res.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.error("There has been error while fetching:", error));
--- my-ts-server/src/index.ts
+++ my-ts-server/src/index.ts
@@ -2,15 +2,16 @@ import * as http from "node:http";
 
 const port = 4444;
 const host = "localhost";
+
 const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.statusCode = 200;
   res.setHeader("contentType", "application/json");
   const data = {
     status: "OK",
-    message:"Yeah!"
-  
-  }
-  
+    message: "Yeah!",
+  };
+
   res.end(JSON.stringify(data));
 });
 
