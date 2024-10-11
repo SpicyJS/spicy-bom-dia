df9ae09cf82d9f83d8c3c029a908ce0ea41c4867
Odin Thif <mjd.thif@icloud.com>
2024-10-11 13:48:51 +0200

    form is submting
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- my-ts-server/src/index.ts
+++ my-ts-server/src/index.ts
@@ -8,24 +8,24 @@ const server = http.createServer(async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.statusCode = 200;
   res.setHeader("contentType", "application/json");
-  const data = await getBody(req)
+  const data = await getBody(req);
+  res.write("fasdfasdf");
 
-  res.end((data));
+  res.end(data);
 });
 
 server.listen(port, host, () => {
   console.log("server is running ");
 });
 
-
 async function getBody(req: http.IncomingMessage) {
-	return await new Promise((resolve) => {
-		let body = "";
-		req.on("data", (chunk) => {
-			body += chunk;
-		});
-		req.on("end", () => {
-			resolve(body);
-		});
-	});
+  return await new Promise((resolve) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      resolve(body);
+    });
+  });
 }
