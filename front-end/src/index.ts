109320880b4046d93eda19ae97f7dcb222ff17c5
Odin Thif <mjd.thif@icloud.com>
2024-10-11 15:20:27 +0200

    fetching the posted data from the from in the front end
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- front-end/src/index.ts
+++ front-end/src/index.ts
@@ -3,25 +3,29 @@ console.log("working on index.js");
 const form = document.getElementById("form");
 const userMsg = document.getElementById("user-messsage")!;
 
-const postData = (name: string) => {
-  fetch("http://localhost:4444", { method: "post", body: name })
+const postData = async (name: string) => {
+  return await fetch("http://localhost:4444", { method: "post", body: name })
     .then((res) => {
       if (!res.ok) throw new Error("Not OK!");
-
-      return res;
+      return res.json();
     })
-    .then((data) => console.log(data))
-    .catch((error) => console.error("There has been error while fetching:", error));
+    .catch(
+      (error) => null
+      // console.error("There has been error while fetching:", error)
+    );
 };
 
 if (form) {
-  form.addEventListener("submit", function (event) {
+  form.addEventListener("submit", async function (event) {
     event.preventDefault();
-    const fullNameElement = document.getElementById("fullname") as HTMLFormElement;
+    const fullNameElement = document.getElementById(
+      "fullname"
+    ) as HTMLFormElement;
     const fullNameValue = fullNameElement.value;
-    userMsg.textContent = `Bom dia ${fullNameValue}!`;
-    console.log(fullNameValue);
-    postData(fullNameValue);
+    userMsg.textContent = await postData(fullNameValue);
+
+    // console.log(fullNameValue);
+
     // form.reset();
   });
 }
