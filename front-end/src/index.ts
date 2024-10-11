882e964710e7ef99c08b39a5875f0f441fe4a87d
Daniel Hormos <daniel_danne32@hotmail.com>
2024-10-11 10:54:36 +0200

    trying to fetch inputted data and displaying it in the div element
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- front-end/src/index.ts
+++ front-end/src/index.ts
@@ -1,15 +1,15 @@
 console.log("working on index.js");
 
-fetch("http://localhost:4444")
-  .then((res) => {
-    if (!res.ok) throw new Error("Not OK!");
+// fetch("http://localhost:4444")
+//   .then((res) => {
+//     if (!res.ok) throw new Error("Not OK!");
 
-    return res.json();
-  })
-  .then((data) => console.log(data))
-  .catch((error) =>
-    console.error("There has been error while fetching:", error)
-  );
+//     return res.json();
+//   })
+//   .then((data) => console.log(data))
+//   .catch((error) =>
+//     console.error("There has been error while fetching:", error)
+//   );
 
 const form = document.getElementById("form");
 const userMsg = document.getElementById("user-messsage");
@@ -17,8 +17,13 @@ const userMsg = document.getElementById("user-messsage");
 if (form) {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    const fullName = document.getElementById("fullname");
-    userMsg?.textContent == `Bom-dia ${fullName}`;
+    const fullNameElement = document.getElementById(
+      "fullname"
+    ) as HTMLFormElement;
+    const fullNameValue = fullNameElement.value;
+    //userMsg?.textContent == `Bom-dia ${fullNameValue}!`;
+    console.log(fullNameValue);
+
     // form.reset();
   });
 }
